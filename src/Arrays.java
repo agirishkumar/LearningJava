85115dc365e0682b9922dca127d548bac2faabcf
agirishkumar <adari.girishkumar@gmail.com>
2023-01-02 20:56:38 -0500

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,7,10};
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
